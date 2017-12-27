524e57e831fc363a81f2640627f6b1ba2a35f260
anton <anton.chasnyk@gmail.com>
2017-12-27 15:30:39 +0200

    PEP8 sugar

--- items/models.py
+++ items/models.py
@@ -11,82 +11,81 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 class MeasureUnit(models.Model):
     """This model represent list of possible measure units"""
-    Name = models.CharField(verbose_name=_('Unit name'), max_length=50, null=False, blank=False, unique=True)
+    name = models.CharField(verbose_name=_('Unit name'), max_length=50, null=False, blank=False, unique=True)
 
     class Meta:
         verbose_name = _('Measure Units')
         verbose_name_plural = _('Measure Units')
-        ordering = 'Name'
+        ordering = 'name'
 
     def __str__(self):
-        return self.Name
+        return self.name
 
 
 class MaterialAsset(models.Model):
-    """This model represent assets at the warehouse. Any asset must have unique Purt Number"""
-    PartNumber = models.CharField(verbose_name=_('Part Number'), max_length=150, null=False,
-                                      blank=False, unique=True)
-    Name = models.CharField(verbose_name=_('Name'), max_length=250, null=False, blank=False)
-    Unit = models.ForeignKey(MeasureUnit, verbose_name=_('Measure Unit'), on_delete=models.PROTECT,
+    """This model represent assets at the warehouse. Any asset must have unique Part Number"""
+    part_number = models.CharField(verbose_name=_('Part Number'), max_length=150, null=False,
+                                   blank=False, unique=True)
+    name = models.CharField(verbose_name=_('Name'), max_length=250, null=False, blank=False)
+    unit = models.ForeignKey(MeasureUnit, verbose_name=_('Measure Unit'), on_delete=models.PROTECT,
                              null=False, blank=False, related_name='MaterialAsset')
-    Description = models.TextField(verbose_name=_('Description'), blank=True, null=True)
+    description = models.TextField(verbose_name=_('Description'), blank=True, null=True)
 
     class Meta:
         verbose_name = _('Material Asset')
         verbose_name_plural = _('Material Assets')
-        ordering = 'PartNumber', 'Name'
+        ordering = 'part_number', 'name'
 
     def __str__(self):
-        return '{}, {} [{}]'.format(self.PartNumber, self.Name, self.Unit.Name)
+        return '{}, {} [{}]'.format(self.part_number, self.name, self.unit.name)
 
 
 class AmountConstraints(models.Model):
     """This model set minimum and max amount for assets for violations and notify"""
-    Asset = models.ForeignKey(MaterialAsset, verbose_name=_('Material Asset'), on_delete=models.PROTECT,
+    asset = models.ForeignKey(MaterialAsset, verbose_name=_('Material Asset'), on_delete=models.PROTECT,
                               related_name='Constraints', unique=True)
     # -1 - mean no minimal limitation
-    _MinAmount = models.BigIntegerField(verbose_name=_('Minimum Amount'), blank=True, null=False, default=-1)
+    _min_amount = models.BigIntegerField(verbose_name=_('Minimum Amount'), blank=True, null=False, default=-1)
     # -1 - mean no maximum limitation
-    _MaxAmount = models.BigIntegerField(verbose_name=_('Maximum Amount'), blank=True, null=False, default=-1)
+    _max_amount = models.BigIntegerField(verbose_name=_('Maximum Amount'), blank=True, null=False, default=-1)
 
     class Meta:
         verbose_name = _('Amount Constraints')
         verbose_name_plural = _('Amount Constraints')
-        ordering = 'Asset'
+        ordering = 'asset'
 
     @property
-    def MinAmount(self):
-        if self._MinAmount < 0:
+    def min_amount(self):
+        if self._min_amount < 0:
             return inf
         else:
-            return self._MinAmount
+            return self._min_amount
 
-    @MinAmount.setter
-    def MinAmount(self, value):
+    @min_amount.setter
+    def min_amount(self, value):
         if isinf(value):
-            self._MinAmount = -1
+            self._min_amount = -1
         else:
-            self._MinAmount = value
+            self._min_amount = value
         self.save()
 
     @property
-    def MaxAmount(self):
-        if self._MaxAmount < 0:
+    def max_amount(self):
+        if self._max_amount < 0:
             return inf
         else:
-            return self._MaxAmount
+            return self._max_amount
 
-
-    @MaxAmount.setter
-    def MaxAmount(self, value):
+    @max_amount.setter
+    def max_amount(self, value):
         if isinf(value):
-            self._MaxAmount = -1
+            self._max_amount = -1
         else:
-            self._MaxAmount = value
+            self._max_amount = value
         self.save()
 
     def __str__(self):
-        return '{} [{}:{}]'.format(self.Asset.PartNumber, self.MinAmount, self.MaxAmount)
+        return '{} [{}:{}]'.format(self.asset.part_number, self.min_amount, self.max_amount)
 
 
 class DocumentType(models.Model):
@@ -95,24 +94,24 @@ class DocumentType(models.Model):
         (-1, _('Out')),
         (1, _('In')),
     )
-    Name = models.CharField(verbose_name=_('Type name'), max_length=50, blank=False, null=False, unique=True)
+    name = models.CharField(verbose_name=_('Type name'), max_length=50, blank=False, null=False, unique=True)
     # Just multiply amount and direction to get correct total amount
-    Direction = models.IntegerField(verbose_name=_('Direction'), choices=TYPES, null=False, blank=False)
+    direction = models.IntegerField(verbose_name=_('Direction'), choices=TYPES, null=False, blank=False)
 
     class Meta:
         verbose_name = _('Document type')
         verbose_name_plural = _('Document types')
-        ordering = 'Name'
+        ordering = 'name'
 
     def __str__(self):
-        return '{} {}'.format(self.Name, dict(self.TYPES)[self.Direction])
+        return '{} {}'.format(self.name, dict(self.TYPES)[self.direction])
 
 
 class Warehouse(MPTTModel):
     """ This model represent several Warehouses.
         This can be a physical warehouse or virtual (transporting for example)
         Warehouse is MPTT model and could be have tree type structure"""
-    Name = models.CharField(verbose_name=_('Warehouse name'), max_length=50, blank=False, null=False, unique=True)
+    name = models.CharField(verbose_name=_('Warehouse name'), max_length=50, blank=False, null=False, unique=True)
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True,
                             on_delete=models.PROTECT)
 
@@ -124,12 +123,13 @@ class Warehouse(MPTTModel):
         order_insertion_by = ['name']
 
     def __str__(self):
-        return self.Name
+        return self.name
 
 
 class Document(models.Model):
-    """ This model represent possible operations this items"""
+    """ This model represent operations with amounts"""
+    type = models.ForeignKey(DocumentType, blank=False, null=False, related_name='document', on_delete=models.PROTECT)
+
     # TODO Подумать о операции перемещения товара между сладами.
     # TODO Создать другую модель с указанием складов (как считать общее количество товара?)
-
-
+    # TODO exam this way do Two documents for moving assets in one transaction.atomic block
